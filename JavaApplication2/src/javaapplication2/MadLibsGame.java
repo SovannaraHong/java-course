a32ef23d913f062b0bb914e9f4613de69a070001
Hong Sovannara <rapewpew372@gmail.com>
2025-09-11 20:28:08 +0700

    u

--- JavaApplication2/src/javaapplication2/MadLibsGame.java
+++ JavaApplication2/src/javaapplication2/MadLibsGame.java
@@ -9,40 +9,41 @@ package javaapplication2;
  * @author ADMIN
  */
 import java.util.Scanner;
+import java.util.Scanner;
 
 public class MadLibsGame {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String abjective1;
-        String abjective2;
         String verb1;
         String verb2;
         String noun;
-        String abjective3;
+        String abjective1;
+        String abjective2;
 
-        System.out.print("Enter Abjective1 :");
-        abjective1 = scanner.nextLine();
-        System.out.print("Enter abjective2 :");
-        abjective2 = scanner.nextLine();
-        System.out.print("Enter noun :");
-        noun = scanner.nextLine();
-        System.out.print("Enter verb1 :");
+        System.out.print("enter verb1 :");
         verb1 = scanner.nextLine();
-        System.out.print("Enter Verb1 :");
+        System.out.print("enter verb1:");
+        verb1 = scanner.nextLine();
+        System.out.print("enter noun:");
+        noun = scanner.nextLine();
+        System.out.print("enter noun:");
+        noun = scanner.nextLine();
+        System.out.print("enter verb2");
         verb2 = scanner.nextLine();
-        System.out.print("Enter abjective3 :");
-        abjective3 = scanner.nextLine();
+        System.out.print("enter abjective1");
+        abjective1 = scanner.nextLine();
+        System.out.print("enter abjective2");
+        abjective2 = scanner.nextLine();
 
-        System.out.println("it Was a " + abjective1 + " cold November Daty");
-        System.out.println("I woke up to the " + abjective2 + "smell of ");
-        System.out.println("roastring in the " + noun);
-        System.out.println(" downstairs. i" + verb1 + "down the stairs to see if i could help ");
-        System.out.println(verb1 + "the dinner My mom said ");
-        System.out.println(abjective3 + "the dinner My mom said ");
-        
+        System.out.println("Hello" + verb1 + "I'm A student at");
+        System.out.println(verb1 + "Institute or university");
+        System.out.println("I study Marjor is" + noun);
+        System.out.println("My subject i like and i love and i want to learn it " + noun);
+        System.out.println("I really love" + verb2);
+        System.out.println("In the freetime i want to play " + abjective1);
+        System.out.println("I dont drink " + abjective2);
         scanner.close();
-        
 
     }
 
