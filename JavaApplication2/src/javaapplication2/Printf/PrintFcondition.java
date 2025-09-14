740c5add9d045860ab0c66248b81f4ffb45e03d4
Hong Sovannara <rapewpew372@gmail.com>
2025-09-14 18:08:06 +0700

    printf

---
+++ JavaApplication2/src/javaapplication2/Printf/PrintFcondition.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication2.Printf;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class PrintFcondition {
+    public static void main(String[] args) {
+        
+        String name ="Hong Sovannra";
+        int age=39;
+        double price = 10.44;
+        char dolla = '$';
+        boolean isStudent =true;
+        
+        System.out.printf("My name is %s\n",name);
+        System.out.printf("My age is %d\n",age);
+        System.out.printf("My Price %f\n",price);
+        System.out.printf("My Dolla is %c\n",dolla);
+        System.out.printf("My Student is %b\n",isStudent);
+                
+    }
+    
+}
