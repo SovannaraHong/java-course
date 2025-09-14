e8e54e4ae10c79355c33338b9db36c75fb73aa62
Hong Sovannara <rapewpew372@gmail.com>
2025-09-14 19:56:09 +0700

    printf output

--- JavaApplication2/src/javaapplication2/Printf/OutputNumber.java
+++ JavaApplication2/src/javaapplication2/Printf/OutputNumber.java
@@ -7,7 +7,21 @@ package javaapplication2.Printf;
 /**
  *
  * @author ADMIN
+ * Note Print if negative number use %(.2f\n have ( it convert to (3049)
  */
 public class OutputNumber {
-    
+
+    public static void main(String[] args) {
+        double price1 = 1000.33;
+        double price2 = 30454.44;
+        double price3 = 10002.99;
+        double price4 =-489.53;
+
+        System.out.printf("Result1 %.2f\n", price1);
+        System.out.printf("Result2 %.2f\n", price2);
+        System.out.printf("Result3 %.2f\n", price3);
+        System.out.printf("Result4 %(.2f\n",price4);
+
+    }
+
 }
